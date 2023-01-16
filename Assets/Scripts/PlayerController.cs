d6d0fd5b10da8c0a5e093c4feef431ba06bc6194
Deividson Sabino <xdeividsabinox@live.com>
2023-01-16 19:57:45 -0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20.0f;
-    private float turnSpeed = 25.0f;
+    [SerializeField] private float speed = 20.0f;
+    private const float turnSpeed = 25.0f;
     private float horizontalInput;
     private float forwardInput;
 
